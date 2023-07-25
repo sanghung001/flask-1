35b5516dcb98cbf345cc64b2f9f584304c69c797
sanghung001 <140505641+sanghung001@users.noreply.github.com>
2023-07-25 19:49:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+if __name__ == '__main__':
+    app.run()
